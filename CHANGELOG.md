3ea7764daa6e33c94f17f72d0ab018bc4e96f831
“Syed <smuh412@gmail.com>
2022-08-02 19:04:39 +0500

    0.0.3 Support for other beacons LED

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-## 0.0.2
+## 0.0.3
+* Support for other beacons LED
 
+## 0.0.2
 * Downgrade meta for stable version
+
+## 0.0.1
+* Initial Release
--- lib/posix_sdk.dart
+++ lib/posix_sdk.dart
@@ -8,11 +8,12 @@ import 'package:flutter_blue/flutter_blue.dart';
 import 'Uuid.dart';
 
 class PosixSdk {
-  late BluetoothCharacteristic? mRXCharacteristic;
   late StreamSubscription<BluetoothDeviceState> deviceState;
   final Uuid _UART_UUID = Uuid.parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+  final Uuid _AUTH_UUID = Uuid.parse("955A152A-0FE2-F5AA-A094-84B8D4F3E8AD");
   final Uuid _UART_RX = Uuid.parse("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
-  final Uuid _UART_TX = Uuid.parse("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+  final Uuid _LED_UUID = Uuid.parse("955A152D-0FE2-F5AA-A094-84B8D4F3E8AD");
+  // final Uuid _UART_TX = Uuid.parse("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
 
   List<int> _getAuthBytes() {
     return hex.decode("FFFF31323334");
@@ -20,6 +21,10 @@ class PosixSdk {
 
   List<int> _getLedBytes() {
     return hex.decode("1502");
+  }
+
+  List<int> _getBytes(String rawHex) {
+    return hex.decode(rawHex);
   }
 
   /// Singleton boilerplate
@@ -31,6 +36,7 @@ class PosixSdk {
 
   /// Establishes a connection to the Bluetooth Device.
   Future<void> connect({required BluetoothDevice device}) async {
+    BluetoothCharacteristic? mRXCharacteristic, mAuthCharacteristic, mLedCharacteristic;
     await device.connect(autoConnect: false);
 
     deviceState = device.state.listen((event) async{
@@ -55,23 +61,31 @@ class PosixSdk {
                       mRXCharacteristic = characteristic;
                     }
                   }
+                }else{
+                  for (var characteristic in service.characteristics) {
+                    print('================================ characteristic ${characteristic.uuid.toString().toUpperCase()} ================================');
+                    if (_AUTH_UUID.toString().toUpperCase() == characteristic.uuid.toString().toUpperCase()) {
+                      print('================================ _AUTH_UUID ${characteristic.uuid.toString().toUpperCase()} ================================');
+                      mAuthCharacteristic = characteristic;
+                    }
+                    if (_LED_UUID.toString().toUpperCase() == characteristic.uuid.toString().toUpperCase()) {
+                      print('================================ _LED_UUID ${characteristic.uuid.toString().toUpperCase()} ================================');
+                      mLedCharacteristic = characteristic;
+                    }
+                  }
                 }
               }
             });
           });
-
-          Timer(const Duration(seconds: 2), () async{
-            if(mRXCharacteristic != null){
-              await mRXCharacteristic!.write(_getAuthBytes(), withoutResponse: true);
-              print('================================ Authenticated ${_getLedBytes().toString()} ================================');
-              await mRXCharacteristic!.write(_getLedBytes(), withoutResponse: true);
-              print('================================ LED ${_getLedBytes().toString()} ================================');
-
-              device.disconnect();
+          Timer(const Duration(seconds: 2), () {
+            if (mRXCharacteristic != null) {
+              _posixLed(mRXCharacteristic!);
+            } else if (mAuthCharacteristic != null && mLedCharacteristic != null) {
+              _otherLed(mAuthCharacteristic!, mLedCharacteristic!);
             }
           });
 
-          Timer(const Duration(seconds: 5), () {
+          Timer(const Duration(seconds: 7), () {
             device.disconnect();
           });
 
@@ -81,5 +95,19 @@ class PosixSdk {
           break;
       }
     });
+  }
+
+  void _posixLed(BluetoothCharacteristic rx) async {
+    await rx.write(_getAuthBytes(), withoutResponse: true);
+    print('================================ Authenticated ${_getLedBytes().toString()} ================================');
+    await rx.write(_getLedBytes(), withoutResponse: true);
+    print('================================ LED ${_getLedBytes().toString()} ================================');
+  }
+
+  void _otherLed(BluetoothCharacteristic auth, BluetoothCharacteristic led) async {
+    await auth.write(_getBytes("29568174"), withoutResponse: true);
+    print('================================ Authenticated ${_getBytes("29568174").toString()} ================================');
+    await led.write(_getBytes("0100"), withoutResponse: true);
+    print('================================ LED ${_getBytes("0100").toString()} ================================');
   }
 }
